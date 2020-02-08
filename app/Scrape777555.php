55d30b7531d9a8cf089f9fbb8277029584e8f106
MaxL1111 <mak1184@mail.ru>
2020-02-08 10:49:54 +0300

    add pagination

--- app/Scrape777555.php
+++ app/Scrape777555.php
@@ -8,37 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class Scrape777555 extends Model
 {
 
-    public $res_pars777555 = array();
-
 
     public function getTelevision()
     {
 
         $client = new Client();
-        //  $guzzleClient = new GuzzleClient(array('timeout' => 60));
 
-        //     $client->setClient($guzzleClient);
-        $crawler = $client->request('SET', 'https://www.777555.by/kompyutery_i_seti/noutbuki_i_aksessuary/noutbuki/');
-        $nodeValuesTitle = $crawler->filter('.catalog-item-title a')->each(function ($node) {
-            //'.catalog-item-title a',
-         //   $this->res_pars777555[] = $node->text() . "\n";
-            return $node->text() . "\n";
-        });
-        $nodeValuesPrice = $crawler->filter('.catalog-item-pricemini')->each(function ($node) {
-            //'.catalog-item-title a',
-            //   $this->res_pars777555[] = $node->text() . "\n";
-            return $node->text() . "\n";
-        });
-        $nodeValues = array_combine($nodeValuesTitle, $nodeValuesPrice);
+        for ($number = 0; $number <= 2; $number++) {
+            // отправляем запрос на сервер,и в качестве результата нам вернулся объект
+            $crawler = $client->request('SET', 'https://www.777555.by/kompyutery_i_seti/noutbuki_i_aksessuary/noutbuki/page' . $number . '/');
+
+            $nodeValuesTitle[] = $crawler->filter('.catalog-item-title a')->each(function ($node) {
+                return $node->text() . "\n";
+            });
+
+
+            $nodeValuesPrice[] = $crawler->filter('.catalog-item-pricemini')->each(function ($node) {
+                return $node->text() . "\n";
+            });
+        }
+
+
+        $i = count($nodeValuesTitle);
+        for ($f = 0; $f < $i; $f++) {
+            foreach ($nodeValuesTitle[$f] as $key => $value) {
+                $nodeValuesTitle2[] = $value;
+            }
+        }
+
+        $i2 = count($nodeValuesPrice);
+        for ($f2 = 0; $f2 < $i2; $f2++) {
+            foreach ($nodeValuesPrice[$f2] as $key => $value) {
+                $nodeValuesPrice2[] = $value;
+            }
+        }
+
+
+        $nodeValues = array_combine($nodeValuesTitle2, $nodeValuesPrice2);
         return $nodeValues;
-    }
-
-
-
-    public function getRes()
-    {
-        $this->getTelevision();
-        return $this->res_pars777555;
     }
 
 
